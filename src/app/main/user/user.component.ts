f23c16da736ef292de7d4e75ebcc5a047cdb446b
LeChiLuc <leluc276@gmail.com>
2017-11-09 21:18:35 +0700

    bai 26

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -1,15 +1,115 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NotificationService } from '../../core/services/notification.service';
+import { DataService } from '../../core/services/data.service';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { MessageContstants } from '../../core/common/message.constants';
+import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+declare var moment: any;
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  @ViewChild('modalAddEdit') public modalAddEdit: ModalDirective;
+  public myRoles: string[] = [];
+  public allRoles: IMultiSelectOption[] = [];
+  public pageIndex: number = 1;
+  public pageSize: number = 20;
+  public pageDisplay: number = 10;
+  public totalRow: number;
+  public filter: string = '';
+  public users: any[];
+  public entity: any;
+  public roles: any[];
+  public dateOptions: any = {
+    locale: { format: 'DD/MM/YYYY' },
+    alwaysShowCalendars: false,
+    singleDatePicker: true
+  };
 
-  constructor() { }
+  constructor(private _dataService: DataService, private _notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.loadRoles();
+    this.loadData();
+  }
+  loadData() {
+    this._dataService.get('/api/appUser/getlistpaging?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
+      .subscribe((response: any) => {
+        this.users = response.Items;
+        this.pageIndex = response.PageIndex;
+        this.pageSize = response.PageSize;
+        this.totalRow = response.TotalRows;
+      });
+  }
+  loadRoles() {
+    this._dataService.get('/api/appRole/getlistall').subscribe((response: any[]) => {
+      this.allRoles = [];
+      for (let role of response) {
+        this.allRoles.push({ id: role.Name, name: role.Description });
+      }
+    }, error => this._dataService.handleError(error));
+  }
+  loadUserDetail(id: any) {
+    this._dataService.get('/api/appUser/detail/' + id)
+      .subscribe((response: any) => {
+        this.entity = response;
+        this.myRoles = [];
+        for (let role of this.entity.Roles) {
+          this.myRoles.push(role);
+        }
+        this.entity.BirthDay = moment(new Date(this.entity.BirthDay)).format('DD/MM/YYYY');
+
+        console.log(this.entity.BirthDay);
+      });
+  }
+  pageChanged(event: any): void {
+    this.pageIndex = event.page;
+    this.loadData();
+  }
+  showAddModal() {
+    this.entity = {};
+    this.modalAddEdit.show();
+  }
+  showEditModal(id: any) {
+    this.loadUserDetail(id);
+    this.modalAddEdit.show();
+  }
+  saveChange(value: boolean) {
+    if (value) {
+      if (this.entity.Id == undefined) {
+        this._dataService.post('/api/appUser/add', JSON.stringify(this.entity))
+          .subscribe((response: any) => {
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageContstants.CREATED_OK_MSG);
+          }, error => this._dataService.handleError(error));
+      }
+      else {
+        this._dataService.put('/api/appUser/update', JSON.stringify(this.entity))
+          .subscribe((response: any) => {
+            this.loadData();
+            this.modalAddEdit.hide();
+            this._notificationService.printSuccessMessage(MessageContstants.UPDATED_OK_MSG);
+          }, error => this._dataService.handleError(error));
+      }
+    }
+  }
+  deleteItem(id: any) {
+    this._notificationService.printConfirmationDialog(MessageContstants.CONFIRM_DELETE_MSG, () => this.deleteItemConfirm(id));
+  }
+  deleteItemConfirm(id: any) {
+    this._dataService.delete('/api/appUser/delete', 'id', id).subscribe((response: Response) => {
+      this._notificationService.printSuccessMessage(MessageContstants.DELETED_OK_MSG);
+      this.loadData();
+    });
+  }
+  public selectGender(event) {
+    this.entity.Gender = event.target.value
   }
 
+  // public selectedDate(value: any) {
+  //   this.entity.BirthDay = moment(value.end._d).format('DD/MM/YYYY');
+  // }
 }
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { Routes, RouterModule } from '@angular/router'
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormsModule } from '@angular/forms';
+import { MultiselectDropdownModule } from 'angular-2-dropdown-multiselect';
+import { Daterangepicker } from 'ng2-daterangepicker';
+
 const userRoutes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index', component: UserComponent }
@@ -9,8 +17,14 @@ const userRoutes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    ModalModule,
+    PaginationModule,
+    FormsModule,
+    MultiselectDropdownModule,
+    Daterangepicker,
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [NotificationService, DataService]
 })
 export class UserModule { }
